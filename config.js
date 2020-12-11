1074df7481a9d1eef92c92d263f4d517b6331f70
Michael Cronin <mcronin_03@yahoo.com>
2020-12-11 18:13:29 -0500

    tes6

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Victor Frankenstein and Imposter Syndrome',
-  subHeading: 'Am I ',
+  subHeading: 'Is Victor ',
 
   // social
   socialLinks: [
