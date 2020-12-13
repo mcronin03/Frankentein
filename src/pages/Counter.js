2dcded75c97ef9bfd6472aa5bd9192f30a70034d
Michael Cronin <mcronin_03@yahoo.com>
2020-12-12 22:30:10 -0500

    predestruction?

--- src/pages/Counter.js
+++ src/pages/Counter.js
@@ -6,56 +6,720 @@ class Counter extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            count: 0
-        }
-        this.state2 = {
-            count: 0
+            count: 0,
+            count2: 0
         }
         this.handleChange = this.handleChange.bind(this)
     }
 
-    increment1() {
-        this.setState({
-            count: this.state.count == 1
-        })
-        console.log(this.state.count)
-    }
+////////////////////////////////////////////
 
-    increment2() {
-        this.setState({
-            count: this.state.count == 2
-        })
-        console.log(this.state.count)
-    }
+increment11() {
+    this.setState({
+        count1: this.state.count1 = 1
+    })
+    console.log(this.state.count1)
+}
 
-    increment3() {
-        this.setState({
-            count: this.state.count + 3
-        })
-        console.log(this.state.count)
-    }
+increment12() {
+    this.setState({
+        count2: this.state.count1 = 2
+    })
+    console.log(this.state.count1)
+}
 
-    increment4() {
-        this.setState({
-            count: this.state.count + 4
-        })
-        console.log(this.state.count)
-    }
+increment13() {
+    this.setState({
+        count2: this.state.count1 = 3
+    })
+    console.log(this.state.count1)
+}
 
-    increment5() {
-        this.setState({
-            count: this.state.count + 5
-        })
-        console.log(this.state.count)
-    }
+increment14() {
+    this.setState({
+        count2: this.state.count1 = 4
+    })
+    console.log(this.state.count1)
+}
 
+increment15() {
+    this.setState({
+        count2: this.state.count1 = 5
+    })
+    console.log(this.state.count1)
+}
+////////////////////////////////////////////
+increment21() {
+    this.setState({
+        count2: this.state.count2 = 1
+    })
+    console.log(this.state.count2)
+}
+
+increment22() {
+    this.setState({
+        count2: this.state.count2 = 2
+    })
+    console.log(this.state.count2)
+}
+
+increment23() {
+    this.setState({
+        count2: this.state.count2 = 3
+    })
+    console.log(this.state.count2)
+}
+
+increment24() {
+    this.setState({
+        count2: this.state.count2 = 4
+    })
+    console.log(this.state.count2)
+}
+
+increment25() {
+    this.setState({
+        count2: this.state.count2 = 5
+    })
+    console.log(this.state.count2)
+}
+////////////////////////////////////////////
+increment31() {
+    this.setState({
+        count3: this.state.count3 = 1
+    })
+    console.log(this.state.count3)
+}
+
+increment32() {
+    this.setState({
+        count2: this.state.count3 = 2
+    })
+    console.log(this.state.count3)
+}
+
+increment33() {
+    this.setState({
+        count2: this.state.count3 = 3
+    })
+    console.log(this.state.count3)
+}
+
+increment34() {
+    this.setState({
+        count2: this.state.count3 = 4
+    })
+    console.log(this.state.count3)
+}
+
+increment35() {
+    this.setState({
+        count2: this.state.count3 = 5
+    })
+    console.log(this.state.count3)
+}
+////////////////////////////////////////////
+increment41() {
+    this.setState({
+        count4: this.state.count4 = 1
+    })
+    console.log(this.state.count4)
+}
+
+increment42() {
+    this.setState({
+        count2: this.state.count4 = 2
+    })
+    console.log(this.state.count4)
+}
+
+increment43() {
+    this.setState({
+        count2: this.state.count4 = 3
+    })
+    console.log(this.state.count4)
+}
+
+increment44() {
+    this.setState({
+        count2: this.state.count4 = 4
+    })
+    console.log(this.state.count4)
+}
+
+increment45() {
+    this.setState({
+        count2: this.state.count4 = 5
+    })
+    console.log(this.state.count4)
+}
+////////////////////////////////////////////
+increment51() {
+    this.setState({
+        count5: this.state.count5 = 1
+    })
+    console.log(this.state.count5)
+}
+
+increment52() {
+    this.setState({
+        count2: this.state.count5 = 2
+    })
+    console.log(this.state.count5)
+}
+
+increment53() {
+    this.setState({
+        count2: this.state.count5 = 3
+    })
+    console.log(this.state.count5)
+}
+
+increment54() {
+    this.setState({
+        count2: this.state.count5 = 4
+    })
+    console.log(this.state.count5)
+}
+
+increment55() {
+    this.setState({
+        count2: this.state.count5 = 5
+    })
+    console.log(this.state.count5)
+}
+////////////////////////////////////////////
+increment61() {
+    this.setState({
+        count6: this.state.count6 = 1
+    })
+    console.log(this.state.count6)
+}
+
+increment62() {
+    this.setState({
+        count2: this.state.count6 = 2
+    })
+    console.log(this.state.count6)
+}
+
+increment63() {
+    this.setState({
+        count2: this.state.count6 = 3
+    })
+    console.log(this.state.count6)
+}
+
+increment64() {
+    this.setState({
+        count2: this.state.count6 = 4
+    })
+    console.log(this.state.count6)
+}
+
+increment65() {
+    this.setState({
+        count2: this.state.count6 = 5
+    })
+    console.log(this.state.count6)
+}
+////////////////////////////////////////////
+increment71() {
+    this.setState({
+        count7: this.state.count7 = 1
+    })
+    console.log(this.state.count7)
+}
+
+increment72() {
+    this.setState({
+        count2: this.state.count7 = 2
+    })
+    console.log(this.state.count7)
+}
+
+increment73() {
+    this.setState({
+        count2: this.state.count7 = 3
+    })
+    console.log(this.state.count7)
+}
+
+increment74() {
+    this.setState({
+        count2: this.state.count7 = 4
+    })
+    console.log(this.state.count7)
+}
+
+increment75() {
+    this.setState({
+        count2: this.state.count7 = 5
+    })
+    console.log(this.state.count7)
+}
+////////////////////////////////////////////
+increment81() {
+    this.setState({
+        count8: this.state.count8 = 1
+    })
+    console.log(this.state.count8)
+}
+
+increment82() {
+    this.setState({
+        count2: this.state.count8 = 2
+    })
+    console.log(this.state.count8)
+}
+
+increment83() {
+    this.setState({
+        count2: this.state.count8 = 3
+    })
+    console.log(this.state.count8)
+}
+
+increment84() {
+    this.setState({
+        count2: this.state.count8 = 4
+    })
+    console.log(this.state.count8)
+}
+
+increment85() {
+    this.setState({
+        count2: this.state.count8 = 5
+    })
+    console.log(this.state.count8)
+}
+////////////////////////////////////////////
+increment91() {
+    this.setState({
+        count9: this.state.count9 = 1
+    })
+    console.log(this.state.count9)
+}
+
+increment92() {
+    this.setState({
+        count2: this.state.count9 = 2
+    })
+    console.log(this.state.count9)
+}
+
+increment93() {
+    this.setState({
+        count2: this.state.count9 = 3
+    })
+    console.log(this.state.count9)
+}
+
+increment94() {
+    this.setState({
+        count2: this.state.count9 = 4
+    })
+    console.log(this.state.count9)
+}
+
+increment95() {
+    this.setState({
+        count2: this.state.count9 = 5
+    })
+    console.log(this.state.count9)
+}
+////////////////////////////////////////////
+increment101() {
+    this.setState({
+        count10: this.state.count10 = 1
+    })
+    console.log(this.state.count10)
+}
+
+increment102() {
+    this.setState({
+        count2: this.state.count10 = 2
+    })
+    console.log(this.state.count10)
+}
+
+increment103() {
+    this.setState({
+        count2: this.state.count10 = 3
+    })
+    console.log(this.state.count10)
+}
+
+increment104() {
+    this.setState({
+        count2: this.state.count10 = 4
+    })
+    console.log(this.state.count10)
+}
+
+increment105() {
+    this.setState({
+        count2: this.state.count10 = 5
+    })
+    console.log(this.state.count10)
+}
+////////////////////////////////////////////
+increment111() {
+    this.setState({
+        count11: this.state.count11 = 1
+    })
+    console.log(this.state.count11)
+}
+
+increment112() {
+    this.setState({
+        count2: this.state.count11 = 2
+    })
+    console.log(this.state.count11)
+}
+
+increment113() {
+    this.setState({
+        count2: this.state.count11 = 3
+    })
+    console.log(this.state.count11)
+}
+
+increment114() {
+    this.setState({
+        count2: this.state.count11 = 4
+    })
+    console.log(this.state.count11)
+}
+
+increment115() {
+    this.setState({
+        count2: this.state.count11 = 5
+    })
+    console.log(this.state.count11)
+}
+////////////////////////////////////////////
+increment121() {
+    this.setState({
+        count12: this.state.count12 = 1
+    })
+    console.log(this.state.count12)
+}
+
+increment122() {
+    this.setState({
+        count2: this.state.count12 = 2
+    })
+    console.log(this.state.count12)
+}
+
+increment123() {
+    this.setState({
+        count2: this.state.count12 = 3
+    })
+    console.log(this.state.count12)
+}
+
+increment124() {
+    this.setState({
+        count2: this.state.count12 = 4
+    })
+    console.log(this.state.count12)
+}
+
+increment125() {
+    this.setState({
+        count2: this.state.count12 = 5
+    })
+    console.log(this.state.count12)
+}
+////////////////////////////////////////////
+increment131() {
+    this.setState({
+        count13: this.state.count13 = 1
+    })
+    console.log(this.state.count13)
+}
+
+increment132() {
+    this.setState({
+        count2: this.state.count13 = 2
+    })
+    console.log(this.state.count13)
+}
+
+increment133() {
+    this.setState({
+        count2: this.state.count13 = 3
+    })
+    console.log(this.state.count13)
+}
+
+increment134() {
+    this.setState({
+        count2: this.state.count13 = 4
+    })
+    console.log(this.state.count13)
+}
+
+increment135() {
+    this.setState({
+        count2: this.state.count13 = 5
+    })
+    console.log(this.state.count13)
+}
+////////////////////////////////////////////
+increment141() {
+    this.setState({
+        count14: this.state.count14 = 1
+    })
+    console.log(this.state.count14)
+}
+
+increment142() {
+    this.setState({
+        count2: this.state.count14 = 2
+    })
+    console.log(this.state.count14)
+}
+
+increment143() {
+    this.setState({
+        count2: this.state.count14 = 3
+    })
+    console.log(this.state.count14)
+}
+
+increment144() {
+    this.setState({
+        count2: this.state.count14 = 4
+    })
+    console.log(this.state.count14)
+}
+
+increment145() {
+    this.setState({
+        count2: this.state.count14 = 5
+    })
+    console.log(this.state.count14)
+}
+////////////////////////////////////////////
+increment151() {
+    this.setState({
+        count15: this.state.count15 = 1
+    })
+    console.log(this.state.count15)
+}
+
+increment152() {
+    this.setState({
+        count2: this.state.count15 = 2
+    })
+    console.log(this.state.count15)
+}
+
+increment153() {
+    this.setState({
+        count2: this.state.count15 = 3
+    })
+    console.log(this.state.count15)
+}
+
+increment154() {
+    this.setState({
+        count2: this.state.count15 = 4
+    })
+    console.log(this.state.count15)
+}
+
+increment155() {
+    this.setState({
+        count2: this.state.count15 = 5
+    })
+    console.log(this.state.count15)
+}
+////////////////////////////////////////////
+increment161() {
+    this.setState({
+        count16: this.state.count16 = 1
+    })
+    console.log(this.state.count16)
+}
+
+increment162() {
+    this.setState({
+        count2: this.state.count16 = 2
+    })
+    console.log(this.state.count16)
+}
+
+increment163() {
+    this.setState({
+        count2: this.state.count16 = 3
+    })
+    console.log(this.state.count16)
+}
+
+increment164() {
+    this.setState({
+        count2: this.state.count16 = 4
+    })
+    console.log(this.state.count16)
+}
+
+increment165() {
+    this.setState({
+        count2: this.state.count16 = 5
+    })
+    console.log(this.state.count16)
+}
+////////////////////////////////////////////
+increment171() {
+    this.setState({
+        count17: this.state.count17 = 1
+    })
+    console.log(this.state.count17)
+}
+
+increment172() {
+    this.setState({
+        count2: this.state.count17 = 2
+    })
+    console.log(this.state.count17)
+}
+
+increment173() {
+    this.setState({
+        count2: this.state.count17 = 3
+    })
+    console.log(this.state.count17)
+}
+
+increment174() {
+    this.setState({
+        count2: this.state.count17 = 4
+    })
+    console.log(this.state.count17)
+}
+
+increment175() {
+    this.setState({
+        count2: this.state.count17 = 5
+    })
+    console.log(this.state.count17)
+}
+////////////////////////////////////////////
+increment181() {
+    this.setState({
+        count18: this.state.count18 = 1
+    })
+    console.log(this.state.count18)
+}
+
+increment182() {
+    this.setState({
+        count2: this.state.count18 = 2
+    })
+    console.log(this.state.count18)
+}
+
+increment183() {
+    this.setState({
+        count2: this.state.count18 = 3
+    })
+    console.log(this.state.count18)
+}
+
+increment184() {
+    this.setState({
+        count2: this.state.count18 = 4
+    })
+    console.log(this.state.count18)
+}
+
+increment185() {
+    this.setState({
+        count2: this.state.count18 = 5
+    })
+    console.log(this.state.count18)
+}
+////////////////////////////////////////////
+increment191() {
+    this.setState({
+        count19: this.state.count19 = 1
+    })
+    console.log(this.state.count19)
+}
+
+increment192() {
+    this.setState({
+        count2: this.state.count19 = 2
+    })
+    console.log(this.state.count19)
+}
+
+increment193() {
+    this.setState({
+        count2: this.state.count19 = 3
+    })
+    console.log(this.state.count19)
+}
+
+increment194() {
+    this.setState({
+        count2: this.state.count19 = 4
+    })
+    console.log(this.state.count19)
+}
+
+increment195() {
+    this.setState({
+        count2: this.state.count19 = 5
+    })
+    console.log(this.state.count19)
+}
+////////////////////////////////////////////
+increment201() {
+    this.setState({
+        count20: this.state.count20 = 1
+    })
+    console.log(this.state.count20)
+}
+
+increment202() {
+    this.setState({
+        count2: this.state.count20 = 2
+    })
+    console.log(this.state.count20)
+}
+
+increment203() {
+    this.setState({
+        count2: this.state.count20 = 3
+    })
+    console.log(this.state.count20)
+}
+
+increment204() {
+    this.setState({
+        count2: this.state.count20 = 4
+    })
+    console.log(this.state.count20)
+}
+
+increment205() {
+    this.setState({
+        count2: this.state.count20 = 5
+    })
+    console.log(this.state.count20)
+}
+ //////////////////////////////////////////
 
     handleChange(event) {
         this.setState({
             count: parseInt(event.target.value)
         });
     }
-
 
     /*
     <button onClick={()=>this.increment1()}>Decrease</button>
@@ -74,39 +738,56 @@ class Counter extends Component {
                 <div className="inner">
                   <h3>Lorem ipsum dolor</h3>
 
-                  <div className="col-4 col-12-small">
-								<input onClick={()=>this.increment1()} type="radio" id="demo-priority-low" name="demo-priority" />
-								<label htmlFor="demo-priority-low">Low</label>
+                            <div className="col-4 col-12-small">
+								<input onChange={()=>this.increment1()} type="radio" id="q1.1" name="demo-priority" />
+								<label htmlFor="q1.1">1</label>
 							</div>
 							<div className="col-4 col-12-small">
-								<input onClick={()=>this.increment2()} type="radio" id="demo-priority-normal" name="demo-priority" />
-								<label htmlFor="demo-priority-normal">Normal</label>
+								<input onChange={()=>this.increment2()} type="radio" id="q1.2" name="demo-priority" />
+								<label htmlFor="q1.2">2</label>
 							</div>
 							<div className="col-4 col-12-small">
-								<input value={()=>this.increment3()} type="radio" id="demo-priority-high" name="demo-priority" />
-								<label htmlFor="demo-priority-high">High</label>
+								<input onChange={()=>this.increment3()} type="radio" id="q1.3" name="demo-priority" />
+								<label htmlFor="q1.3">3</label>
 							</div>
-                            
+                            <div className="col-4 col-12-small">
+								<input onChange={()=>this.increment4()} type="radio" id="q1.4" name="demo-priority" />
+								<label htmlFor="q1.4">4</label>
+							</div>
+							<div className="col-4 col-12-small">
+								<input onChange={()=>this.increment5()} type="radio" id="q1.5" name="demo-priority" />
+								<label htmlFor="q1.5">5</label>
+							</div>
+
                             <h3>Lorem ipsum dolor</h3>
-
-                  <div className="col-4 col-12-small">
-								<input onClick={()=>this.increment1()} type="radio" id="demo-priority-low" name="demo-priority" />
-								<label htmlFor="demo-priority-low">Low</label>
+                            <div className="col-4 col-12-small">
+								<input onChange={()=>this.increment21()} type="radio" id="q2.1" name="2demo-priority" />
+								<label htmlFor="q2.1">1</label>
 							</div>
 							<div className="col-4 col-12-small">
-								<input onClick={()=>this.increment2()} type="radio" id="demo-priority-normal" name="demo-priority" />
-								<label htmlFor="demo-priority-normal">Normal</label>
+								<input onChange={()=>this.increment22()} type="radio" id="q2.2" name="2demo-priority" />
+								<label htmlFor="q2.2">2</label>
 							</div>
 							<div className="col-4 col-12-small">
-								<input value={()=>this.increment3()} type="radio" id="demo-priority-high" name="demo-priority" />
-								<label htmlFor="demo-priority-high">High</label>
+								<input onChange={()=>this.increment23()} type="radio" id="q2.3" name="2demo-priority" />
+								<label htmlFor="q2.3">3</label>
 							</div>
-
+                            <div className="col-4 col-12-small">
+								<input onChange={()=>this.increment24()} type="radio" id="q2.4" name="2demo-priority" />
+								<label htmlFor="q2.4">4</label>
+							</div>
+							<div className="col-4 col-12-small">
+								<input onChange={()=>this.increment25()} type="radio" id="q2.5" name="2demo-priority" />
+								<label htmlFor="q2.5">5</label>
+                            </div>
+                            
 
 
                     <button onClick={()=>this.increment1()}>Decrease</button>
                         <div>Count - {this.state.count}</div>
-                    <button onClick={() => this.increment1()}>Increment</button>
+                        <div>Count - {this.state.count2}</div>
+                        {this.state.count + this.state.count2 === 1 && <span>show something</span>}
+                    <button>Increment</button>
                   <p>
                     Morbi mattis mi consectetur tortor elementum, varius pellentesque
                     velit convallis. Aenean tincidunt lectus auctor mauris maximus, ac
