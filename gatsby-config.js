63fbf9a25d0f20758be0c809363183a3822bdc7a
Michael Cronin <mcronin_03@yahoo.com>
2021-01-08 14:06:28 -0500

    new gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,13 +21,25 @@ module.exports = {
     },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline',
-    //
+    // DEC 2020
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingId: "UA-144910986-2",
         head: true,
+      },
+    },
+    // JAN 2021
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-HJ8DHXKWYS", // Google Analytics / GA
+          "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
       },
     },
     //
