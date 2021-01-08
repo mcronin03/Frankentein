5b381edb787440d8a6fad4666b91e247bb811632
Michael Cronin <mcronin_03@yahoo.com>
2021-01-08 14:47:15 -0500

    added Frank 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,22 +26,9 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingId: "UA-144910986-2",
+        trackingId: "UA-144910986-3",
         head: true,
       },
     },
-    // JAN 2021
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "G-HJ8DHXKWYS", // Google Analytics / GA
-          //"AW-CONVERSION_ID", // Google Ads / Adwords / AW
-          //"DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
-        ],
-      },
-    },
-    //
   ],
 };
