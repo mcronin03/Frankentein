33904f03f9207357d17e410ded007ca158cd76cc
Michael Cronin <mcronin_03@yahoo.com>
2021-01-08 14:57:06 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,18 @@ module.exports = {
         head: true,
       },
     },
+    //JAN 2021
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-ZNWMKC0Y9N", // Google Analytics / GA
+          //"AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          //"DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+      },
+    },
+    //
   ],
 };
